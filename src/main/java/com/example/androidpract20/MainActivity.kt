e986ba84ff60929c6fca56a889ec30ba78baf9f7
ClassZak <156465942+ClassZak@users.noreply.github.com>
2024-11-20 12:05:31 +0300

    college commit

--- src/main/java/com/example/androidpract20/MainActivity.kt
+++ src/main/java/com/example/androidpract20/MainActivity.kt
@@ -1,26 +1,24 @@
 package com.example.androidpract20
 
-import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlinx.coroutines.Runnable
-import kotlinx.coroutines.delay
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
-    var threadWorks:Boolean=false
-    var giroTextViewRef:TextView?=null
+class MainActivity : AppCompatActivity() {
+    private var threadWorks: Boolean = false
+    private var haveGyroscope: Boolean = true
+    private var giroTextViewRef: TextView? = null
+    private var sensor: Sensor? = null
 
     private var angleX = 0.0
     private var angleY = 0.0
@@ -28,60 +26,89 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private var lastTimestamp: Long = 0
 
+    private lateinit var sensorManager: SensorManager
+    private var updateThread: Thread? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        giroTextViewRef=findViewById<TextView>(R.id.giroTextView) as TextView?
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        giroTextViewRef = findViewById(R.id.giroTextView)
+
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        if (sensor == null) {
+            haveGyroscope = false
+            Toast.makeText(this, "У вас нет гироскопа", Toast.LENGTH_LONG).show()
+        }
     }
 
-    fun onGiroThreadMageButtonClick(view: View){
-        val buttonRef:Button=view as Button
+    fun onGiroThreadMageButtonClick(view: View) {
+        if (!haveGyroscope) {
+            Toast.makeText(this, "У вас нет гироскопа", Toast.LENGTH_LONG).show()
+            return
+        }
 
-        if(!threadWorks){
-            buttonRef.setText(getString(R.string.start_giro_thread))
+        val buttonRef: Button = view as Button
 
-            Thread.sleep(500)
-            var action:Runnable=Runnable{
-                while (threadWorks){
-                    angleX+=4
-                    giroTextViewRef?.post{
-                        giroTextViewRef?.setText(getString(R.string.axis_angle_descriptiom)+" x:$angleX\n"+getString(R.string.axis_angle_descriptiom)+" y:$angleY\n"+getString(R.string.axis_angle_descriptiom)+" z:$angleZ")
+        if (!threadWorks) {
+            buttonRef.text = getString(R.string.stop_giro_thread)
+
+            // Регистрируем слушатель гироскопа
+            sensorManager.registerListener(listenerGyroscope, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+
+            // Запускаем поток для обновления UI
+            threadWorks = true
+            updateThread = Thread {
+                while (threadWorks) {
+                    runOnUiThread {
+                        giroTextViewRef?.text = getString(R.string.axis_angle_descriptiom) +
+                                " x: %d°\n".format(Math.toDegrees(angleX).toInt()%360) +
+                                getString(R.string.axis_angle_descriptiom) + " y: %d°\n".format(Math.toDegrees(angleY).toInt()%360) +
+                                getString(R.string.axis_angle_descriptiom) + " z: %d°".format(Math.toDegrees(angleZ).toInt()%360)
                     }
-                    Thread.sleep(495)
+                    Thread.sleep(500)
                 }
             }
-            Thread(action).start()
-        }
-        else{
-            buttonRef.setText(getString(R.string.stop_giro_thread))
-        }
-
-        threadWorks=!threadWorks
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (event != null && event.sensor.type == Sensor.TYPE_GYROSCOPE) {
-            val deltaTime = (event.timestamp - lastTimestamp) / 1_000_000_000.0 // наносекунды в секунды
-            lastTimestamp = event.timestamp
-
-            val deltaX = event.values[0] * deltaTime // угловая скорость по X
-            val deltaY = event.values[1] * deltaTime // угловая скорость по Y
-            val deltaZ = event.values[2] * deltaTime // угловая скорость по Z
-
-            // Обновляем углы
-            angleX += deltaX
-            angleY += deltaY
-            angleZ += deltaZ
+            updateThread?.start()
+        } else {
+            buttonRef.text = getString(R.string.start_giro_thread)
+            sensorManager.unregisterListener(listenerGyroscope)
+            threadWorks = false
+            updateThread?.join()
         }
     }
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        TODO("Not yet implemented")
+    override fun onPause() {
+        super.onPause()
+        if (haveGyroscope) {
+            sensorManager.unregisterListener(listenerGyroscope)
+        }
+        threadWorks = false
+        updateThread?.interrupt()
+    }
+
+    private val listenerGyroscope: SensorEventListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
+        override fun onSensorChanged(event: SensorEvent) {
+            if (event.sensor.type == Sensor.TYPE_GYROSCOPE) {
+                val deltaTime = (event.timestamp - lastTimestamp) / 1_000_000_000.0 // наносекунды в секунды
+                lastTimestamp = event.timestamp
+
+                if (deltaTime > 0) {
+                    angleX += event.values[0] * deltaTime
+                    angleY += event.values[1] * deltaTime
+                    angleZ += event.values[2] * deltaTime
+                }
+            }
+        }
     }
 }
